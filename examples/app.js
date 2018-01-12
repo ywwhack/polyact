0a4640ac706a109695861c53b616c42a63107321
ywwhack <yww7289@163.com>
2018-01-12 15:49:40 +0800

    feat: add created hook

--- examples/app.js
+++ examples/app.js
@@ -5,10 +5,24 @@ const map = new AMap.Map('app', {
   zoom: 14,
   center: new AMap.LngLat(121.485360083494, 31.2798373003168)
 })
+const infoWindow = new AMap.InfoWindow()
 const polyact = new Polyact(map)
 
 polyact.render({
   type: 'grid',
   data,
-  colors: ['#b1e4fe', '#7ed3fc', '#47c2Fa', '#17b5f9', '#00a8f7', '#009ae8']
+  colors: ['#b1e4fe', '#7ed3fc', '#47c2Fa', '#17b5f9', '#00a8f7', '#009ae8'],
+  created (result) {
+    result.forEach(({
+      polygon,
+      data
+    }) => {
+      polygon.on('click', () => {
+        infoWindow.open(map, polygon.getBounds().getCenter())
+        infoWindow.setContent(`
+          <div style="padding: 40px 80px;">区域数量：${data.count}</div>
+        `)
+      })
+    })
+  }
 })
